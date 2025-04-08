ef217cd9e2b6430db4eb8ba948bbb187200a5b2c
盛岡/MoRi <154046421+MoRi0s@users.noreply.github.com>
2025-04-08 21:20:55 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-// ハンバーガーメニューをトグルする関数
-document.getElementById('hamburger-menu').addEventListener('click', function() {
-    const navMenu = document.getElementById('nav-menu');
-    const hamburger = document.getElementById('hamburger-menu');
-    
-    // メニューの表示/非表示を切り替え
-    navMenu.classList.toggle('active');
-    
-    // ハンバーガーの状態（閉じる/開く）をトグル
-    hamburger.classList.toggle('open');
-});
+
